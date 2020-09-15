bcbb32144479336c5f2e3f05601848667e3b976d
Melindadiaz07 <melindadiaz@me.com>
2020-09-15 17:05:00 -0400

    Added photo url to bird

--- our-pet-store/db/seeds.rb
+++ our-pet-store/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.delete_all
 
 Pet.create(name: "Pluto", species: "Dog", year_of_birth: 2020, good_with_kids: true)
 Pet.create(name: "Nugget", species: "Dog", year_of_birth: 2019, good_with_kids: true)
@@ -16,4 +17,4 @@ Pet.create(name: "Whiskers", species: "Cat", year_of_birth: 2019, good_with_kids
 Pet.create(name: "Penny", species: "Cat", year_of_birth: 2015, good_with_kids: false)
 Pet.create(name: "Franklin", species: "Cat", year_of_birth: 2018, good_with_kids: true)
 
-Pet.create(name: "Polly", species: "Bird", year_of_birth: 2010, good_with_kids: true)
+Pet.create(name: "Polly", species: "Bird", year_of_birth: 2010, good_with_kids: true, image_url:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-pet-birds-parakeet-1572839068.jpg")
